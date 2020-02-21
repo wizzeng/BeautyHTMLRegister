326fcbf579640a9f07d956b056b8b4ee3220ae97
Zinc <244462375@qq.com>
2020-02-21 16:18:58 +0800

    :sparkles:完成页面+逻辑

---
+++ readme.md
@@ -0,0 +1,12 @@
+# BeautyRegister HTML
+
+使用 JQuery + Validate.js 构建的还算优雅的用户注册页面，某HTML课程作业.
+
+![ScreenShot](./md/screenshot.png)
+
+## 作业要求
+制定一个注册页面（包含字段（必须）：用户账号、密码、确认密码、性别、地址）
+
+1.	有用到 validate.js 表单校验、
+2.	提交按钮 边上 增加 取消按钮利用 JQuery 的val属性 取消 input 文本框的内容
+3.	用到CSS里面的 background 颜色，比如：注册页面的背景色或者字体的颜色等等
---
+++ src/js/main.js
@@ -0,0 +1,114 @@
+$().ready(() => {
+    addFocusListener();
+    $('#register-btn').on('click', ()=>{
+        validateForm();
+        if(errorStack.length === 0){
+            alert('提交成功');
+        }
+    });
+    validateForm()
+});
+
+let errorStack = [];
+
+function validateForm() {
+    $('#register-form').validate({
+        onkeyup: () => {
+            // 禁用 onInput 事件的 validate
+        },
+        errorPlacement: function (error, element) {
+            let inputItem = element.parent();
+            errorStack = error;
+            setTimeout(() => {
+                let tag = inputItem.children('span.error-text');
+                if (tag.length !== 0) {
+                    tag[0].classList = 'error-text error-text-left';
+                    setTimeout(() => {
+                        for (let i = 0; i < tag.length; i++) {
+                            tag[i].remove();
+                        }
+                        mountErrorText(inputItem, error[0].innerText);
+                    }, 500)
+                } else {
+                    mountErrorText(inputItem, error[0].innerText);
+                }
+            }, 500)
+        },
+        rules: {
+            username: {
+                required: true,
+                minlength: 8
+            },
+            password: {
+                required: true,
+                minlength: 8
+            },
+            'pass-confirm': {
+                required: true,
+                equalTo: '#password'
+            },
+            email: {
+                required: true,
+                email: true,
+            },
+            address: {
+                required: true,
+                minlength: 10
+            }
+        },
+        messages: {
+            username: {
+                required: '请输入用户名',
+                minlength: '用户名不少于8位'
+            },
+            password: {
+                required: '请输入密码',
+                minlength: '密码不少于8位'
+            },
+            'pass-confirm': {
+                required: '请输入确认密码',
+                equalTo: '两次输入的密码不相同'
+            },
+            email: {
+                required: '请输入Email',
+                email: 'Email 格式不正确'
+            },
+            address: {
+                required: '请输入地址',
+                minlength: '地址长度至少为10位'
+            }
+        }
+    })
+}
+
+function mountErrorText(target, text) {
+    const errorTag = document.createElement('span');
+    errorTag.innerText = text;
+    errorTag.classList = 'error-text';
+    target.attr('class', 'input-item input-item-error');
+    target.append(errorTag)
+}
+
+function addFocusListener() {
+    let inputBoxGroup = $('.input-item');
+    let inputGroup = $('.input-item input');
+    let backupPlaceholder = {};
+    for (let i = 0; i < inputGroup.length; i++) {
+        inputGroup[i].onfocus = () => {
+            inputBoxGroup[i].classList = 'input-item input-item-focus';
+            backupPlaceholder[i] = (inputGroup[i].placeholder);
+            inputGroup[i].placeholder = ''
+        };
+        inputGroup[i].onchange = (event)=>{
+            if(event.target.value !== ''){
+                inputBoxGroup[i].classList = 'input-item input-item-focus';
+            }
+        };
+        inputGroup[i].onblur = (event) => {
+            if(event.target.value === ''){
+                inputBoxGroup[i].classList = 'input-item';
+                inputGroup[i].placeholder = backupPlaceholder[i];
+            }
+        }
+    }
+}
